abf4ad7c4f4e2f829ea29f94bd82acba564efb2b
BashaVali <bashavali222@gmail.com>
2022-10-26 06:11:21 +0530

    [Basha]Add:Project Folder Structure

---
+++ EmployeeWageProblem/EmployeeWage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageProblem
+{
+    public class EmployeeWage
+    {
+        const int WAGE_PER_HR = 20, FULL_DAY_HR = 8, IS_PRESENT = 0, IS_FULL_TIME = 1;
+        Random random = new Random();
+        public void Attendence()
+        {
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_PRESENT)
+                Console.WriteLine("Employee is Present");
+            else
+                Console.WriteLine("Employee is Absent");
+        }
+        public void EmpWage()
+        {
+            int dailyEmpWage = 0, empHrs = 0;
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+                empHrs = FULL_DAY_HR;
+            else
+                empHrs = 0;
+            dailyEmpWage = WAGE_PER_HR * empHrs;
+            Console.WriteLine("Daily Emp Wage" + dailyEmpWage);
+        }
+    }
+
+}
---
+++ EmployeeWageProblem/Program.cs
@@ -0,0 +1,16 @@
+﻿using EmployeeWageProblem;
+using System;
+
+namespace EmployeeWageProblem
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Welcome to Employee wage statement");
+
+            EmployeeWage employeeWage = new EmployeeWage();
+            employeeWage.Attendence();
+        }
+    }
+}
